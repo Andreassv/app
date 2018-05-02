4ac80ae88588672318bec0c3b83049d0c0ee3528
Andreassv <35996622+Andreassv@users.noreply.github.com>
2018-05-02 12:22:23 +0200

    Update js.js

--- js.js
+++ js.js
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     function flipTile(event) {
         var tiles = Array.from(board.querySelectorAll('section'));
-        if (event.target !== event.currentTarget && envent.touches.length === 1) {
+        if (event.target !== event.currentTarget && event.touches.length === 1) {
             if (!event.target.parentNode.classList.contains('flipped')) {
                 event.target.parentNode.classList.add('flipped');
                 tilesFlipped.push(tiles.indexOf(event.target.parentNode));
